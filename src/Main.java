cb8828a501e3ba1d5674a44d8ef214a053903d35
lucas <lucas@asia.edu.tw>
2017-09-12 15:18:46 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int i;
+        System.out.print("Input i=");
+        i=sc.nextInt();
+        System.out.println("i="+i);
+        System.out.println("This project is OK!");
+        for(i=1;i<=10;i++)
+        {
+            //System.out.println(i);
+            System.out.printf("%d",i);
+        }
+	// write your code here
+    }
+}
